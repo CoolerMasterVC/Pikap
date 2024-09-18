e30be8a7b5e5d25fe2abdd37869e59ae0928a9c9
Чиканчи Владислав <mr.vlad-2005@yandex.ru>
2024-09-18 16:00:40 +0300

    Create Program.cs

--- C_sharp/lab1/ConsoleApp1/ConsoleApp1/Program.cs
+++ C_sharp/lab1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -35,7 +35,7 @@ namespace Squed
                 {
                     Console.WriteLine("Введите коэффициент b:");
                     b_check = Console.ReadLine();
-                    
+
                     if (double.TryParse(b_check, out b))
                     {
                         break;
@@ -49,7 +49,7 @@ namespace Squed
                 {
                     Console.WriteLine("Введите коэффициент c:");
                     c_check = Console.ReadLine();
-                    
+
                     if (double.TryParse(c_check, out c))
                     {
                         break;
@@ -63,6 +63,11 @@ namespace Squed
 
             D = b * b - 4 * a * c;
 
+            double t1 = -1;
+            double t2 = -1;
+
+
+            double x1 = -100000, x2 = -100000, x3 = -100000, x4 = -100000;
             if (D < 0)
             {
                 Console.ForegroundColor = ConsoleColor.Red;
@@ -70,21 +75,72 @@ namespace Squed
             }
             else
             {
-                Console.ForegroundColor = ConsoleColor.Green;
 
 
                 if (D == 0)
                 {
-                    Console.WriteLine("Корень данного уравнения: {0}", (-b) / (2 * a));
+                    t1 = (-b) / (2 * a);
+                    //Console.WriteLine("Корень данного уравнения: {0}", t1);
+                    if (t1 >= 0)
+                    {
+                        x1 = Math.Sqrt(t1);
+                        x2 = -Math.Sqrt(t1);
+
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Корни: {0}, {1}",x1,x2);
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Нет действительных корней");
+                    }
+                    
                 }
                 else
                 {
+                    bool flag = false;
                     D = Math.Sqrt(D);
-                    Console.WriteLine("Корни данного квадратного уравнения: {0}; {1}",
-                        (-b + D) / (2 * a), (-b - D) / (2 * a));
+                    t1 = (-b + D) / (2 * a);
+                    t2 = (-b - D) / (2 * a);
+                    //Console.WriteLine("Корни данного квадратного уравнения: {0}; {1}",
+                    //(-b + D) / (2 * a), (-b - D) / (2 * a));
 
+
+                    if (t1 >= 0)
+                    {
+                        flag = true;
+                        x1 = Math.Sqrt(t1);
+                        x2 = -Math.Sqrt(t1);
+                    }
+                    if (t2 >= 0)
+                    {
+                        flag = true;
+                        x3 = Math.Sqrt(t2);
+                        x4 = -Math.Sqrt(t2);
+                    }
+                    if (flag)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write("Корни: ");
+                        if (x1 != -100000)
+                        {
+                            Console.Write("{0}, {1} ", x1, x2);
+                        }
+                        if (x3 != -100000)
+                        {
+                            Console.Write("{0}, {1} ", x3, x4);
+                        }
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Нет действительных корней");
+                    }
                 }
             }
+
+
+
             //Console.WriteLine("{0},{1},{2},{3}",a, b, c,D);
             Console.ResetColor();
             return 0;
