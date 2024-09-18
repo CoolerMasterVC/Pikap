d248ed8c9b6fdd8a41c946c42649887034e39ed3
Чиканчи Владислав <mr.vlad-2005@yandex.ru>
2024-09-18 15:38:32 +0300

    Create Program.cs

---
+++ C_sharp/lab1/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+namespace Squed
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            double a, b, c;
+            double D = -1;
+            if (args.Length == 3)
+            {
+                a = Convert.ToDouble(args[0]);
+                b = Convert.ToDouble(args[1]);
+                c = Convert.ToDouble(args[2]);
+            }
+            else
+            {
+                string a_check, b_check, c_check;
+                while (true)
+                {
+                    Console.WriteLine("Введите коэффициент а: ");
+                    a_check = Console.ReadLine();
+                    if (double.TryParse(a_check, out a))
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Неверный ввод ");
+                    }
+                }
+                while (true)
+                {
+                    Console.WriteLine("Введите коэффициент b:");
+                    b_check = Console.ReadLine();
+                    
+                    if (double.TryParse(b_check, out b))
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Неверный ввод.");
+                    }
+                }
+                while (true)
+                {
+                    Console.WriteLine("Введите коэффициент c:");
+                    c_check = Console.ReadLine();
+                    
+                    if (double.TryParse(c_check, out c))
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Неверный ввод.");
+                    }
+                }
+            }
+
+            D = b * b - 4 * a * c;
+
+            if (D < 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Нет действительных корней");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+
+
+                if (D == 0)
+                {
+                    Console.WriteLine("Корень данного уравнения: {0}", (-b) / (2 * a));
+                }
+                else
+                {
+                    D = Math.Sqrt(D);
+                    Console.WriteLine("Корни данного квадратного уравнения: {0}; {1}",
+                        (-b + D) / (2 * a), (-b - D) / (2 * a));
+
+                }
+            }
+            //Console.WriteLine("{0},{1},{2},{3}",a, b, c,D);
+            Console.ResetColor();
+            return 0;
+        }
+    }
+}
